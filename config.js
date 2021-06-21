a0ed51ff302568ca8356c7967f719ccafb48700d
tidic84 <tidic84@gmail.com>
2021-06-21 22:38:51 +0200

    fix kick, ban + /config prefix

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  prefix: "k/",
   MONGODB_SRV: 'mongodb+srv://kallium_root:kallium_root_password@kallium.g9dn0.mongodb.net/DiscordBotDB?retryWrites=true&w=majority',
   DEFAULTSETTINGS: {
     prefix: "k/",
