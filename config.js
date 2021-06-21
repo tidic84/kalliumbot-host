68136689ce141f76367342cddc5a31650650784f
tidic84 <tidic84@gmail.com>
2021-06-21 22:45:26 +0200

    db link

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  MONGODB_SRV: 'mongodb+srv://kallium_root:kallium_root_password@kallium.g9dn0.mongodb.net/DiscordBotDB?retryWrites=true&w=majority',
+  MONGODB_SRV: process.env.DB_LINK,
   DEFAULTSETTINGS: {
     prefix: "k/",
     welcomeMessage: 'Welcome {{user}}'
