d95aa8a735b3009a2ef4ca7812e31a480447381a
tidic84 <tidic84@gmail.com>
2021-06-22 18:44:24 +0200

    big fix play

--- config.js
+++ config.js
@@ -1,8 +1,11 @@
 module.exports = {
+  token: "ODU1NTQ1NDM2MzgyMTY3MDgw.YM0Cxw.OzKmaGWJpJQ-zVn77iePMR2Zvng",
   MONGODB_SRV: process.env.DB_LINK,
   DEFAULTSETTINGS: {
-    prefix: "k/",
-    welcomeMessage: 'Welcome {{user}}'
+    prefix: "!",
+    welcomeMessage: "Bienvenue **{{USER}}** !\n Amuse toi bien sur **{{SERVER_NAME}}** :tada:",
+    leaveMessage: "Aurevoir **{{USER}}**\n On espère te revoir bientot :cry:"
+
   }
 }
 
